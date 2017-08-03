717602f98260c91effc78348c7649ae07fb2f366
lunax <albertofly10@gmail.com>
2017-08-03 23:48:25 +0200

    Try jose.4.j implementation of JWT

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,96 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
 import java.security.Key;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.RSAKeyProvider;
 import io.jsonwebtoken.*;
+import org.jose4j.jwk.RsaJsonWebKey;
+import org.jose4j.jwk.RsaJwkGenerator;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.lang.JoseException;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 
 public class Main {
 
-
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         System.out.println("hi!");
 
-        //The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.ES256;
+        //
+        // JSON Web Token is a compact URL-safe means of representing claims/attributes to be transferred between two parties.
+        // This example demonstrates producing and consuming a signed JWT
+        //
 
-        long nowMillis = System.currentTimeMillis();
+        // Generate an RSA key pair, which will be used for signing and verification of the JWT, wrapped in a JWK
+        RsaJsonWebKey rsaJsonWebKey = null;
+        try {
+            rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
+        } catch (JoseException e) {
+            e.printStackTrace();
+        }
 
-        long expiry = 1501944516000L;
+        // Give the JWK a Key ID (kid), which is just the polite thing to do
+        rsaJsonWebKey.setKeyId("k1");
 
-        Key signKey = KeyGenerator.getInstance("MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg247w7fZYc27Dr/cBJc5laTJFwLJLgs9jQaSeUfVU1kygCgYIKoZIzj0DAQehRANCAATOLyYZpBnAweJPU/FG4j0oA/z/qLTS7OJ5P839h9Rtngfs564at6azXK7udrYsTkRVZcpCJ7H5P8cPELcF7uUQ").generateKey();
+        // Create the Claims, which will be the content of the JWT
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("Issuer");  // who creates the token and signs it
+        claims.setAudience("Audience"); // to whom the token is intended to be sent
+        claims.setExpirationTimeMinutesInTheFuture(10); // time when the token will expire (10 minutes from now)
+        claims.setGeneratedJwtId(); // a unique identifier for the token
+        claims.setIssuedAtToNow();  // when the token was issued/created (now)
+        claims.setNotBeforeMinutesInThePast(2); // time before which the token is not yet valid (2 minutes ago)
+        claims.setSubject("subject"); // the subject/principal is whom the token is about
+        claims.setClaim("email","mail@example.com"); // additional claims/attributes about the subject can be added
+        List<String> groups = Arrays.asList("group-one", "other-group", "group-three");
+        claims.setStringListClaim("groups", groups); // multi-valued claims work too and will end up as a JSON array
+
+        // A JWT is a JWS and/or a JWE with JSON claims as the payload.
+        // In this example it is a JWS so we create a JsonWebSignature object.
+        JsonWebSignature jws = new JsonWebSignature();
+
+        // The payload of the JWS is JSON content of the JWT Claims
+        jws.setPayload(claims.toJson());
+
+        // The JWT is signed using the private key
+        jws.setKey(rsaJsonWebKey.getPrivateKey());
+
+        // Set the Key ID (kid) header because it's just the polite thing to do.
+        // We only have one key in this example but a using a Key ID helps
+        // facilitate a smooth key rollover process
+        jws.setKeyIdHeaderValue(rsaJsonWebKey.getKeyId());
+
+        // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+
+        // Sign the JWS and produce the compact serialization or the complete JWT/JWS
+        // representation, which is a string consisting of three dot ('.') separated
+        // base64url-encoded parts in the form Header.Payload.Signature
+        // If you wanted to encrypt it, you can simply set this jwt as the payload
+        // of a JsonWebEncryption object and set the cty (Content Type) header to "jwt".
+        String jwt = null;
+        try {
+            jwt = jws.getCompactSerialization();
+        } catch (JoseException e) {
+            e.printStackTrace();
+        }
 
 
-        long expiryDate = 150194400;
-        Date now = new Date(nowMillis);
-        Date exp = new Date(expiryDate);
+        // Now you can do something with the JWT. Like send it to some other party
+        // over the clouds and through the interwebs.
+        System.out.println("JWT: " + jwt);
 
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder()
-                .setIssuedAt(now)
-                .setHeaderParam("alg","ES256")
-                .setHeaderParam("kid","B93K9Z88NU")
-                .setExpiration(exp)
-                .setIssuer("B68385H95A")
-                .signWith(signatureAlgorithm, signKey);
-
-
-        System.out.println("BUILDER: " + builder.compact());
 
 
     }
